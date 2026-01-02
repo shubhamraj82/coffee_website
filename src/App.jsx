a6267a41bbcccfb67af9cb9bbe6672f7b87b360f
shubhamraj82 <raj.shubh8216@gmail.com>
2026-01-02 22:42:33 +0530

    Hero Section animation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
+import { ScrollTrigger } from 'gsap/all';
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <NavBar/>
+      <HeroSection/>
+      <div className='h-dvh border-amber-500'></div>
+    </div>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='fixed top-0 left-0 md:p-9 p-3'>
+        <img src="/images/nav-logo.svg" alt="nav-logo" className='md:w-24 w-20'/>
+    </nav>
+  )
+}
+export default NavBar
